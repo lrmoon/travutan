9ac5a3ea995b1c9352e640910c5ec569319cb6dc
ManliestBen <benjamintmanley@gmail.com>
2021-08-08 21:11:34 -0500

    add profile resource

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,22 @@
 import { User } from '../models/user.js'
+import { Profile } from '../models/profile.js'
 
 export {
   signup
 }
 
-function signup(req, res) {
+async function signup(req, res) {
+  const profile = new Profile(req.body)
+  req.body.profile = profile._id
   const user = new User(req.body)
-  user.save()
-  .then(user =>{
+  try {
+    await user.save();
+    await profile.save();
+
     // TODO: Send back a JWT instead of the user
     res.status(200).json(user)
-  })
-  .catch(err => {
+  
+  } catch (err) {
     res.status(400).send({ err: err.errmsg })
-  })
+  }
 }
