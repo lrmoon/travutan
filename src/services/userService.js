62d918e9334e4f3fd9aa1a5d528de57af67a329a
lrmoon <larariad@rocketmail.com>
2021-11-05 14:58:15 -0400

    finish template

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,13 @@
 import * as tokenService from "../services/tokenService";
-const BASE_URL = '/api/users/';
+const BASE_URL = 'http://localhost:3001/api/users/';
 
 export function getAllUsers() {
-    return fetch(BASE_URL, {}, {mode: "cors"})
-        .then(res => res.json())
-}
+    return fetch(
+      BASE_URL,
+      {
+        headers: { Authorization: "Bearer " + tokenService.getToken() },
+      },
+      { mode: "cors" }
+    ).then((res) => res.json())
+  }
 
