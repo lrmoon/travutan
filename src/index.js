630e248133aacc6d6b8fccc42088539555b3b99c
lrmoon <larariad@rocketmail.com>
2021-11-05 10:34:11 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App props="stuff"/>
+    <App props="things"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
