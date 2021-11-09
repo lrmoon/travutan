856d98b53bc26e4319ea95d345fa5bce9c067689
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-09 16:58:37 -0500

    add Link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -8,34 +8,26 @@ const NavBar = () => {
 	return (
 		<>
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    {/* <a className="navbar-brand" href="#">Navbar</a> */}
-    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button> */}
-    {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
-      {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0"> */}
-        <li className="nav-item">
-          <a className="nav-link" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Destination</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">SignUp</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Login</a>
-        </li>
-      {/* </ul> */}
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    {/* </div> */}
-  </div>
-</nav>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+					<li className="nav-item navElement">
+					<a className="nav-link" href="#">Home</a>
+					</li>
+					<li className="nav-item navElement">
+					<a className="nav-link" href="#">Destination</a>
+					</li>
+					<li className="nav-item navElement">
+					<a className="nav-link" href="#">SignUp</a>
+					</li>
+					<li>
+						<Link to="/login">Log In</Link>
+					</li>
+				<form className="d-flex">
+					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+					<button className="btn btn-outline-success" type="submit">Search</button>
+				</form>
+			</div>
+			</nav>
 
 			{/* {user ? (
 				<nav className={styles.userNav}>
