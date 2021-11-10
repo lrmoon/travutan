58f48f17691eebdcd012075577c4a4e7fe5137b2
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-10 15:11:18 -0500

    add reply schema

--- models/questionPost.js
+++ models/questionPost.js
@@ -2,9 +2,21 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema;
 
+const replySchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId, ref: "Profile"
+    },
+    content: String,
+})
+
 const questionPostSchema = new Schema({
     content: String,
-    
+    author: { type: Schema.Types.ObjectId, ref: "Profile"},
+    replies: [replySchema],
+    destination: {
+        type: Schema.Types.ObjectId, ref:'Destination'
+    }
+
   },{
     timestamps: true,
   });
