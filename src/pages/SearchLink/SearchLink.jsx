170e0bd1d2f8338140a0eab33980a670c3c6c64a
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-09 21:42:45 -0500

    added state with urlid of selected city to be fetched

--- src/pages/SearchLink/SearchLink.jsx
+++ src/pages/SearchLink/SearchLink.jsx
@@ -2,11 +2,17 @@ import * as roadgoatService from './../../services/roadgoatService'
 import React, {Component} from 'react'
 
 class SearchLink extends Component{
+
+    state = {
+        urlId: this.props.location.state.val.id,
+        cityDetails: {}
+    }
     
     render(){
-        console.log(this.props.location.state.val);
+        console.log(this.props.location.state.val.id);
+        const {val} = this.props.location.state
         return(
-            <h1>{this.props.location.state.val.attributes.name}</h1>
+            <h1>{val.attributes.name}</h1>
         )        
     }
 
