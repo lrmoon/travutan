9b9831c8d912ece649b61621bf64ff843b9b1b41
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-10 11:46:31 -0500

    set the text decoration

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({ user, handleLogout, handleChange, handleSubmit, searchTitle })
 			{user ? (
 				<nav className="navbar navbar-expand-lg navbar-light bg-light">
 					<div className="container-fluid">
-						<li className={styles.navElement}>TRAVUTAN</li>
+						<li><Link className={styles.navElement} to="/">TRAVUTAN</Link></li>
 						<li><Link className={styles.navElement} to="/users">Users</Link></li>
 						<li><Link className={styles.navElement} to='/destinations'>Destinations</Link></li>
 						<li><Link className={styles.navElement} to='' onClick={handleLogout}>Log Out</Link></li>
@@ -21,7 +21,7 @@ const NavBar = ({ user, handleLogout, handleChange, handleSubmit, searchTitle })
 				</nav>
 			) : (
 				<nav className="navbar navbar-expand-lg navbar-light bg-light">
-					<div className="container-fluid">
+					<div className="container-fluid removeLine">
 						<li><Link className={styles.navElement} to="/">TRAVUTAN</Link></li>
 						<li><Link className={styles.navElement} to='/destinations'>Destinations</Link></li>
 						<li><Link className={styles.navElement} to="/signup">Sign Up</Link></li>
