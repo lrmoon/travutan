30b608eb07e924d579c60961858ef69d9e05b512
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-09 20:48:48 -0500

    remove useless code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,31 +4,8 @@ import styles from './NavBar.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const NavBar = ({ user, handleLogout, handleChange, handleSubmit }) => {
-	// top = { user, handleLogout, handleChange, handleSubmit }
 	return (
 		<>
-
-			{/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container-fluid">
-					<li className="navElement">
-						<Link to="/">Home</Link>
-					</li>
-					<li className="navElement">
-						<Link to='/destinations'>Destinations</Link>
-					</li>
-					<li className="navElement">
-						<Link to="/signup">Sign Up</Link>
-					</li>
-					<li className="navElement">
-						<Link to="/login">Log In</Link>
-					</li>
-				<form onSubmit={handleSubmit} className="d-flex">
-					<input onChange={handleChange} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-					<button value="Submit" className="btn btn-outline-dark" type="submit">Search</button>
-				</form>
-			</div>
-			</nav> */}
-
 			{user ? (
 				<nav className={styles.userNav}>
 						<ul>
@@ -72,10 +49,7 @@ const NavBar = ({ user, handleLogout, handleChange, handleSubmit }) => {
 							</form>
 					</div>
 				</nav>
-
-			
-			)
-			}
+			)}
 		</>
 	)
 }
