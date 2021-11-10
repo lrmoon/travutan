a503b958ed142f6257e356b8d75b43a081773892
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-10 14:48:14 -0500

    add wikiUrl

--- models/destination.js
+++ models/destination.js
@@ -6,6 +6,7 @@ const destinationSchema = new Schema({
     title: String,
     image: String,
     covid: String,
+    wikiUrl: String,
     reviews: [{ type: Schema.Types.ObjectId, ref: "destinationReview" }]
   },{
     timestamps: true,
