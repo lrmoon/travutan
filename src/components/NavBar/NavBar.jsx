ba5abfe673bee87c0fe826fad8d7cfe0e7e12b28
lrmoon <larariad@rocketmail.com>
2021-11-11 10:58:48 -0500

    user navbar added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,9 +7,14 @@ const NavBar = ({ user, handleLogout, handleChange, handleSubmit, searchTitle })
 	return (
 		<>
 			{user ? (
-				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<nav className="navbar navbar-expand-lg navbar-white bg-white">
 					<div className="container-fluid">
-						<li><Link styles={{fontSize:'30px'}} className={styles.navElement} to="/">TRAVUTAN</Link></li>
+						<div>
+							<span>
+								<img src='/images/travutan_icon.png' alt="icon" style={{height:"100px", width: "100px", margin: "0px"}}></img>
+							</span>
+							<Link className={styles.navElement} style={{fontSize: "50px", margin: "0px"}} to="/">TRAVUTAN</Link>
+						</div>
 						<li><Link className={styles.navElement} to="/users">Users</Link></li>
 						<li><Link className={styles.navElement} to='/destinations'>Destinations</Link></li>
 						<li><Link className={styles.navElement} to='' onClick={handleLogout}>Log Out</Link></li>
