f75775e6eb9653304c021c9c188c1c6ad928edd4
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-11 21:25:27 -0500

    added a conditonal for undefined data to return a loading page untilrerender

--- src/pages/SearchShow/SearchShow.jsx
+++ src/pages/SearchShow/SearchShow.jsx
@@ -8,7 +8,9 @@ class SearchShow extends Component{
         loginApi: process.env.REACT_APP_APIKEY,
         passwordApi: process.env.REACT_APP_SECRETKEY,
         urlId: this.props.location.state.val.id,
-        cityDetails: {}
+        cityDetails: {},
+        // form body to be used when handling submit and setting state
+        formBody: {}
     }
     
     async componentDidMount(){
@@ -18,14 +20,13 @@ class SearchShow extends Component{
         this.setState({
             cityDetails: cityJson.data.attributes,
             cityIncluded: cityJson.included
-            // set state of cityimages from cityJson included.images
         })
-
 
     }
 
     handleSubmit = e => {
         e.preventDefault();
+
         console.log('handle submit')
     }
 
@@ -33,39 +34,48 @@ class SearchShow extends Component{
         
         const {cityDetails, cityIncluded} = this.state
 
-        // drilling covid info
-        const covidData = cityDetails.covid;
-        const firstCovidLocal = cityDetails.covid ? Object.keys(covidData)[0] : ''  
+        // if covid is defined render our page, else render loading
+        if(cityDetails.covid){
+            // drilling covid info
+            const covidData = cityDetails.covid;
+            const firstCovidLocal = cityDetails.covid ? Object.keys(covidData)[0] : null  
 
-        // drilling budget info
-        const budgetData = cityDetails.budget;
-        const firstBudgetLocal = cityDetails.budget ? Object.keys(budgetData)[0] : ''
+            // drilling budget info
+            const budgetData = cityDetails.budget;
+            const firstBudgetLocal = cityDetails.budget ? Object.keys(budgetData)[0] : null
 
-        // drilling image info
-        const photosArray = cityIncluded ? cityIncluded.filter((photo) => photo.type === 'photo'): ''
-        const lastPhoto = photosArray ? photosArray[photosArray.length -1].attributes.image.full : ''
-        
-        return(
-            <div className='show'>
-                <h1>{cityDetails.long_name}</h1>
-                <img width='200px' src={lastPhoto} alt='comingsoon'></img>
-                <p>{`Budget: ${cityDetails.budget ? cityDetails.budget[firstBudgetLocal].text : ''}`}</p>
-                <p>Population: {cityDetails.population}</p><br/>
-                <a href={cityDetails.wikipedia_url}>Wiki</a>
-                <p>{`Covid Level: ${cityDetails.covid ? cityDetails.covid[firstCovidLocal].text : ''}`}</p>
+            // drilling image info
+            const photosArray = cityIncluded ? cityIncluded.filter((photo) => photo.type === 'photo'): null
+            const lastPhoto = photosArray ? photosArray[photosArray.length -1].attributes.image.full : null
+            
+            return(
+                <div className='show'>
 
-                <form onSubmit={this.handleSubmit}>
-                    <input hidden type='text' value={cityDetails.long_name} name="title"/>
-                    <input hidden type='text' value={lastPhoto} name='image'/>
-                    <input hidden type='text' value={cityDetails.budget[firstBudgetLocal].text} name='budget'/>
-                    <input hidden type='number' value={cityDetails.population} name='population'/>
-                    <input hidden type='text' value={cityDetails.wikipedia_url} name='wikiUrl'/>
-                    <input hidden type='text' value={cityDetails.covid[firstCovidLocal].text} name='covid'/>
-                    <button>Add to collection</button>    
-                </form>
-                
-            </div>
-        )        
+                    <h1>{cityDetails.long_name}</h1>
+                    <img width='200px' src={lastPhoto} alt='comingsoon'></img>
+                    <p>{`Budget: ${cityDetails.budget ? cityDetails.budget[firstBudgetLocal].text : ''}`}</p>
+                    <p>Population: {cityDetails.population}</p><br/>
+                    <a href={cityDetails.wikipedia_url}>Wiki</a>
+                    <p>{`Covid Level: ${cityDetails.covid ? cityDetails.covid[firstCovidLocal].text : ''}`}</p>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <input hidden type='text' value={cityDetails.long_name} name="title"/>
+                        <input hidden type='text' value={lastPhoto} name='image'/>
+                        <input hidden type='text' value={cityDetails.budget ? cityDetails.budget[firstBudgetLocal].text : null} name='budget'/>
+                        <input hidden type='number' value={cityDetails.population} name='population'/>
+                        <input hidden type='text' value={cityDetails.wikipedia_url} name='wikiUrl'/>
+                        <input hidden type='text' value={cityDetails.covid ? cityDetails.covid[firstCovidLocal].text : null} name='covid'/>
+                        <button>Add to collection</button>    
+                    </form>
+                    
+
+                </div>
+
+            )          
+        } else {
+            return <h1>Loading...</h1>
+        }
+          
     }
 
 }
