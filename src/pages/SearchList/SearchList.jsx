3df1feb73baa37836fd0b0873ec135aec1b62699
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-13 17:50:58 -0500

    style the search list and h1

--- src/pages/SearchList/SearchList.jsx
+++ src/pages/SearchList/SearchList.jsx
@@ -1,33 +1,32 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
-import styles from '../Login/Login.module.css'
+import styles from '../Landing/Landing.module.css'
 
 class SearchList extends Component{
 
     render(){
         return(
-            <div className={styles.container}>
+            <div className={styles.searchList}>
                 <h1> Search List</h1>
-                    {
-                        this.props.search.data ?
-                        this.props.search.data.map(val => (
-                            // only render destination types that are cities
-                            val.attributes.destination_type === 'City' ?
-                            <div key={val.id} className='searchDivs'>
-                                
-                                <Link to={{
-                                    pathname: '/searchShow',
-                                    state: {val}
-                                }}>
-                                {val.attributes.name} {val.id}
-                                </Link>                    
-                            </div>
-                            :
-                            ''
-                        ))
-                        : 
+                {
+                    this.props.search.data ?
+                    this.props.search.data.map(val => (
+                        // only render destination types that are cities
+                        val.attributes.destination_type === 'City' ?
+                        <div key={val.id} className='searchDivs'>   
+                            <Link to={{
+                                pathname: '/searchShow',
+                                state: {val}
+                            }}>
+                            {val.attributes.name}
+                            </Link>                    
+                        </div>
+                        :
                         ''
-                    }                
+                    ))
+                    : 
+                    ''
+                }                
             </div>
         )
     }
