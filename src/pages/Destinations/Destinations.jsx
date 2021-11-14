9c95c6c4ba772113499861d2669505b46c5d9a90
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-14 15:57:54 -0500

    added profiles destination page

--- src/pages/Destinations/Destinations.jsx
+++ src/pages/Destinations/Destinations.jsx
@@ -1,22 +1,32 @@
 import styles from './Destinations.module.css'
 
-const Destinations = ({profile, profileDestinations}) => {
-  console.log(profile)
+const Destinations = ({profile}) => {
+  console.log(profile ? profile : '')
   return (
     <main className={styles.container}>
       <h1>
          {profile ? profile.name : ""}'s Favorite Destinations
       </h1>
+
+      
       <div className={styles.flexContainer}>
         {
-          profileDestinations.map((profileDestination, idx) => (
+          profile.destinations ? 
+
+          profile.destinations.map((profileDestination, idx) => (
             <div className={styles.destinationCard} key={idx}>
               <h4>{profileDestination.title}</h4>
               <img width="90%" src={profileDestination.image} alt='soon'/>
             </div>
           ))
+
+          :
+
+          ''
         }
       </div>
+
+
     </main>
   )
 }
