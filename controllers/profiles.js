8979283a9da0a40fc8ede0231741731c5872ad85
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-13 19:58:06 -0500

    added backend findby id of profile and console logged

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,27 +2,11 @@ import { Profile } from "../models/profile.js"
 import {Destination} from "../models/destination.js"
 
 function show(req, res) {
-    console.log(req.user)
-    // Find the profile that was clicked
-    Profile.findById(req.params.id)
-    // Populate friends to get profile data for each of them
-    .populate('destinations')
-    .then(profile => {
-      // Use the profile clicked to find games belonging to that user
-      Destination.find({ collectedBy: profile._id })
-      .then(destinations => {
-        // Find the profile of the current logged in user
-        Profile.findById(req.user.profile)
-        .then(userProfile => {
-          res.json(userProfile)
-        })
-      })
   
-    })
-    .catch(err => {
-      console.log(err)
-      res.redirect('/')
-    })
+    console.log(req.user.profile)
+    Profile.findById(req.user.profile)
+    .then(profileRes => console.log(profileRes))
+
   }
 
 
