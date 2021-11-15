3ffb90e893e0bdf46a16cbcb56f961fd4759b232
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-14 20:17:46 -0500

    added a form body using inputs

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,19 +1,51 @@
+import React, {Component} from 'react'
 
-function EditPage(props){
-    const {profileDestination} = props.location.state
-    console.log(profileDestination);
 
-    const handleEdit = async formBody => {
-        console.log(formBody)
+class EditPage extends Component{
+
+    state = {
+        destination: this.props.location.state.profileDestination,
+        form: {},
+        editTitle: 'a'
     }
 
-    return (
-        <div className='formClass'>
-            <form onSubmit={this.handleEdit}>
-                <button type='submit'>Update</button>
-            </form>
-        </div>
-    )
+    handleChange = (e) => {
+        this.setState({
+            editTitle: e.target.value
+        })
+    }
+
+    handleEdit = async e => {
+        e.preventDefault()
+
+        const {image, covid, budget, population, wikiUrl} = this.state.destination
+
+        const formBody = {
+            title: this.state.editTitle,
+            image,
+            covid,
+            budget,
+            population,
+            wikiUrl
+        }
+
+        console.log(formBody);
+    }
+
+
+    render() {
+        const {destination, editTitle} = this.state;
+        console.log(destination)
+        return (
+            <div className='formClass'>
+                <form onSubmit={this.handleEdit}>
+                    <input type='text' name='title' value={editTitle} onChange={this.handleChange}/>
+                    <button type='submit'>Update</button>
+                </form>
+            </div>
+        )        
+    }
+
 }
 
 export default EditPage
